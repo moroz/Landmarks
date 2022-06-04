7f70105d930a6ed60bd645aab24b7741253e4d3f
Karol Moroz <karol@moroz.dev>
2022-06-05 00:28:40 +0200

    Put components together in a view

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -8,7 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
+  var body: some View {
+    VStack {
+      MapView()
+        .ignoresSafeArea(edges: .top)
+        .frame(height: 300)
+
+      CircleImage()
+        .offset(y: -130)
+        .padding(.bottom, -130)
+
       VStack(alignment: .leading) {
         Text("Turtle Rock")
           .font(.title)
@@ -21,11 +30,14 @@ struct ContentView: View {
         }
       }
       .padding()
+
+      Spacer()
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
