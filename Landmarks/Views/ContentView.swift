508ddad66d5b7b864cf1490719e6b96caa7b88e6
Karol Moroz <karol@moroz.dev>
2022-06-05 00:54:44 +0200

    Add images, build a Landmark model to fetch data

--- Landmarks/Views/ContentView.swift
+++ Landmarks/Views/ContentView.swift
@@ -23,11 +23,17 @@ struct ContentView: View {
           .font(.title)
         HStack {
           Text("Joshua Tree National Park")
-            .font(.subheadline)
           Spacer()
           Text("California")
-            .font(.subheadline)
         }
+        .font(.subheadline)
+        .foregroundColor(.secondary)
+        
+        Divider()
+        
+        Text("About Turtle Rock")
+          .font(.title2)
+        Text("Descriptive text goes here.")
       }
       .padding()
 
