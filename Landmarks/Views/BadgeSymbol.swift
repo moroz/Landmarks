97686ede5e8be456b4a02070336af72360d06742
Karol Moroz <karol@moroz.dev>
2022-06-08 01:03:06 +0200

    Add icons, start drawing the mountain symbol

---
+++ Landmarks/Views/BadgeSymbol.swift
@@ -0,0 +1,37 @@
+//
+//  BadgeSymbol.swift
+//  Landmarks
+//
+//  Created by Karol Moroz on 08/06/2022.
+//
+
+import SwiftUI
+
+struct BadgeSymbol: View {
+  var body: some View {
+    GeometryReader { geometry in
+      Path { path in
+        let width = min(geometry.size.width, geometry.size.height)
+        let height = width * 0.75
+        let spacing = width * 0.03
+        let middle = width * 0.5
+        let topWidth = width * 0.226
+        let topHeight = height * 0.488
+
+        path.addLines([
+          CGPoint(x: middle, y: spacing),
+          CGPoint(x: middle - topWidth, y: topHeight - spacing),
+          CGPoint(x: middle, y: topHeight / 2 + spacing),
+          CGPoint(x: middle + topWidth, y: topHeight - spacing),
+          CGPoint(x: middle, y: spacing),
+        ])
+      }
+    }
+  }
+}
+
+struct BadgeSymbol_Previews: PreviewProvider {
+  static var previews: some View {
+    BadgeSymbol()
+  }
+}
